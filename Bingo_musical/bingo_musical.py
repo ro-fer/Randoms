464051b6ed6c8cdc528ba5a6da291de609e8e726
Rocio Fernandez <97984182+ro-fer@users.noreply.github.com>
2025-06-10 08:10:27 -0300

    Update bingo_musical.py

--- Bingo_musical/bingo_musical.py
+++ Bingo_musical/bingo_musical.py
@@ -4,7 +4,7 @@ Programa que ingresando un listado de canciones, cantidad de cartones y cantidad
 import random 
 def bingo ( canciones , cant_cartones, cant_canciones):
     canciones = canciones.split(";")
-    for i in range(1,cant_cartones):
+    for i in range(1,cant_cartones+1):
         print(f"Carton {i}: \n  {random.sample(canciones,cant_canciones)}")
 
 
